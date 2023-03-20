d1c0ee11b8a668c0db87bf7f83ec85ea69269bc2
s24937 <s24937@pjwstk.edu.pl>
2023-03-20 20:26:12 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,148 @@
+import random
+#papier kamien
+import getpass
+print (random.randint(0, 10))
+choice = getpass.getpass("wybor:")
+print(choice)
+
+
+#temperature = float(input("pomiar temperatury"))
+# if temperature < 28 or temperature > 41:
+#     print ("uwaga! zagrożenie życia "+ str(temperature))
+# else:
+#     print("temp "+ str(temperature))
+#
+
+# preassure = "180/110"
+# symptom = "ból w klatce piersiowej"
+#
+# my_preassure= float(input("pomiar cisnienia"))
+# my_symptom = float(input("objawy"))
+#
+# if my_preassure == preassure or my_symptom == symptom:
+#     print("trzeba wezwac karetke")
+# else:
+#     print("niebezpieczenstwo")
+#
+
+
+
+# while True:
+#     dane = input("podaj dane / end - konczy: ")
+#     if dane == "end":
+#         break
+#     print(dane)
+#
+#     i = 0
+#     while i < 10
+#         print(i)
+#         i+=1
+#
+# i = 0
+# while i < 10
+#     if i == 5:
+#         break
+#     print(i)
+#     i+=1
+#
+# i = 0
+# while i < 10
+#     i += 1
+#     if i-1 == 5;
+#         # i += 1
+#         continue
+#     print(i-1)
+
+#
+# for i in range(0, 10):
+#     print(i)
+#
+# for i in range(0, 10,2):
+#     print(i)
+#
+# for i in range(0, 10):
+#     if i == 5:
+#         break
+#     print(i)
+#
+# moja_lista = ["warszawa", "kraków", "wroclaw", "lodz"]
+# print(len(moja_lista))
+#
+# print(moja_lista[len(moja_lista)-1])
+#
+# for index in range (0, len(moja_lista)):
+#     print(moja_lista[index])
+#
+# moja_lista[index]= "plock"
+# print(moja_lista)
+#
+# moja_lista.append("Sopot")
+# print(moja_lista)
+#
+# moja_lista.insert(0, "Katowice")
+# print(moja_lista)
+#
+#
+# moja_lista.extend(["nowe", "nowe"])
+# print(moja_lista)
+#
+# #usunie index z konca
+# del moja_lista[-1]
+# print(moja_lista)
+#
+# del moja_lista[len(moja_lista)-1]
+# print(moja_lista)
+#
+# #z konca stosu
+# miasto= moja_lista.pop()
+# print(miasto)
+#
+# index = moja_lista.index("wroclaw")
+# miasto = moja_lista.pop(index)
+# print(miasto)
+#
+# print(moja_lista)
+#
+#
+# miasto= "warszawaaaaaa"
+# if miasto in moja_lista:
+#     moja_lista.remove("warszawaaaaaa")
+# else:
+#     print("nie ma w liscie takiego miasta")
+#
+# print(moja_lista)
+# moja_lista.append("Warszawa")
+# moja_lista.insert(0, "Warszawa")
+#
+#
+# while miasto in moja_lista:
+#     moja_lista.remove()
+# print(moja_lista)
+#
+#
+# print(max(moja_lista))
+# print(min(moja_lista))
+#
+# moja_lista.append("Warszawa")
+# moja_lista.append("Łódz")
+# print(max(moja_lista))
+# print(min(moja_lista))
+#
+# liczby = list(range(0, 10, 1))
+# for element in liczby:
+#     print(element)
+#
+# print(max(liczby))
+# print(min(liczby))
+#
+# cities = ""
+#
+
+#lista 2 wymiarowa
+
+lista2d = [[], []]
+
+
+
+
+
